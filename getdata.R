49c42d807025708f7e275755952ba130cff79aca
Daniel Felbah <paadanfel@hotmail.com>
2019-10-27 20:08:35 -0400

    wrapped around the get_data function

--- getdata.R
+++ getdata.R
@@ -1,17 +1,19 @@
-library(lubridate)
-# download file
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "power.zip")
-
-unzip("power.zip")
-
-data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?", stringsAsFactors = FALSE)
-
-data$Date <- strftime(as.Date(data$Date, format = "%d/%m/%Y"))
-
-data <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
-
-data$DateTime <- paste(data$Date, data$Time)
-
-data$DateTime <- ymd_hms(data$DateTime)
+get_data <- function(){
+  library(lubridate)
+  # download file
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "power.zip")
+  
+  unzip("power.zip")
+  
+  data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?", stringsAsFactors = FALSE)
+  
+  data$Date <- strftime(as.Date(data$Date, format = "%d/%m/%Y"))
+  
+  data <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+  
+  data$DateTime <- paste(data$Date, data$Time)
+  
+  data$DateTime <- ymd_hms(data$DateTime)
+}
 
 
